59da32ea7c99f150064cfe99f8e8b954302608d0
gaetanok <ok.gaetan@gmail.com>
2019-08-16 16:54:33 +0200

    navbar done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,6 +21,14 @@ class CocktailsController < ApplicationController
     end
   end
 
+
+ def destroy
+      @cocktail = Cocktail.find(params[:id])
+      @cocktail.destroy
+      redirect_to cocktails_path
+    end
+
+
   def cocktail_params
     params.require(:cocktail).permit(:name, :photo)
   end
